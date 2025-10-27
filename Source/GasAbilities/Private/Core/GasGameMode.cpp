d22ea94c1c537f76e137e3ae2642515f9f7a52d7
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-10-27 19:43:57 +0100

    Add Game Mode Base

---
+++ Source/GasAbilities/Private/Core/GasGameMode.cpp
@@ -0,0 +1,3 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "GasAbilities/Public/Core/GasGameMode.h"
---
+++ Source/GasAbilities/Public/Core/GasGameMode.h
@@ -0,0 +1,19 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GasGameMode.generated.h"
+
+/**
+ * AGasGameMode is the primary Game Mode for the GasAbilities project. 
+ * It handles the fundamental rules of the game, including the default Pawn, and setting up the initial classes.
+ * This class is designed to provide the necessary game context and rules for the project utilizing the
+ * Unreal Engine's Gameplay Ability System (GAS).
+*/
+UCLASS()
+class GASABILITIES_API AGasGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
