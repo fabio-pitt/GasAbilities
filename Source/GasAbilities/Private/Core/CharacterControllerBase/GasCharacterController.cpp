9e0407c75fb14657de1de4d0e89626e01543b2b6
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-10-29 11:54:35 +0100

    feat: add character controller

---
+++ Source/GasAbilities/Private/Core/CharacterControllerBase/GasCharacterController.cpp
@@ -0,0 +1,30 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "GasCharacterController.h"
+
+// Sets default values
+AGasCharacterController::AGasCharacterController()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGasCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGasCharacterController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AGasCharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/GasAbilities/Private/Core/CharacterControllerBase/GasCharacterController.h
@@ -0,0 +1,35 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GasCharacterController.generated.h"
+
+/**
+ * AGasCharacterController is the Player Controller used for the main playable character in the project.
+ * It is responsible for handling player input and **binding Ability inputs** to the 
+ * Character's Gameplay Ability System (GAS). 
+ * This controller manages the flow of commands and replication necessary for the player to utilize Abilities,
+ * while also setting up the initial Player State and HUD for the GAS environment.
+ */
+UCLASS()
+class GASABILITIES_API AGasCharacterController : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGasCharacterController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
