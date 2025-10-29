a2f965c10da04c6ba0bc49c17d45855f9fa51d00
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-10-29 18:47:39 +0100

    feat: enhance GasCharacterController with input handling
    - Add reference and binding InputAction objects
    - Handle Character Possesion
    - Handle functions to Look, Move, Jump

--- Source/GasAbilities/Private/Core/CharacterControllerBase/GasCharacterController.h
+++ Source/GasAbilities/Private/Core/CharacterControllerBase/GasCharacterController.h
@@ -3,8 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
+#include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 #include "GasCharacterController.generated.h"
+
+class AGasCharacterBase;
+struct FInputActionValue;
 
 /**
  * AGasCharacterController is the Player Controller used for the main playable character in the project.
@@ -14,22 +19,51 @@
  * while also setting up the initial Player State and HUD for the GAS environment.
  */
 UCLASS()
-class GASABILITIES_API AGasCharacterController : public ACharacter
+class GASABILITIES_API AGasCharacterController : public APlayerController
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AGasCharacterController();
+	// The Input Mapping Context that defines the input scheme for the character.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = GAS_Input, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputMappingContext> CurrentMappingContext;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	// Reference to the current Gas Character Base controlled by this controller.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = GAS_Input, meta = (AllowPrivateAccess = true))
+	TObjectPtr<AGasCharacterBase> CurrentCharacter;
+
+	/// Declare input actions for movement and looking around
+
+	// Input action for looking around
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = GAS_Input, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputAction> LockAction;
+
+	// Input action for moving
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = GAS_Input, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputAction> MoveAction;
+
+	// Input action for jumping
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = GAS_Input, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UInputAction> JumpAction;
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupInputComponent() override;
+
+	// Called when the controlled is possessing a pawn
+	virtual void OnPossess(APawn* InPawn) override;
+
+	/// Input actions handlers
+
+	// Handler for Look action
+	void Look(const FInputActionValue& Value);
+
+	// Handler for Move action
+	void Move(const FInputActionValue& Value);
+
+	// Handler for Jump action start
+	void JumpStart();
+
+	// Handler for Jump action stop
+	void JumpStop();
 };
--- Source/GasAbilities/Private/Core/GasGameMode.cpp
+++ Source/GasAbilities/Private/Core/GasGameMode.cpp
@@ -6,12 +6,23 @@
 AGasGameMode::AGasGameMode()
 {
 	// Set the default pawn class to our Blueprinted character
-	// Note: Update the path to match the Blueprint location
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Characters/BP_GasCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT(
+		"/Game/Blueprints/Characters/BP_MainCharacter"));
 
 	// If the Blueprint class is found, set it as the default pawn class
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	// Set the default player controller class to our custom controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT(
+		"/Game/Blueprints/Characters/BP_MainController"));
+
+	// If the Blueprint class is found, set it as the default player controller class
+	if (PlayerControllerBPClass.Class != nullptr)
+	{
+		PlayerControllerClass = PlayerPawnBPClass.Class;
+	}
+	else UE_LOG(LogTemp, Warning, TEXT("PlayerController class not found"));
 }
