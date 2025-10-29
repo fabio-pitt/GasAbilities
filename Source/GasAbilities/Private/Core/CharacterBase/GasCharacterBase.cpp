30e25ad9864533e3d5662fad04d14d78aede4f39
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-10-29 11:37:44 +0100

    refactor(core): moved GasCharacterBase in private folder

---
+++ Source/GasAbilities/Private/Core/CharacterBase/GasCharacterBase.cpp
@@ -0,0 +1,30 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "Core/GasCharacterBase.h"
+
+// Sets default values
+AGasCharacterBase::AGasCharacterBase() :
+	CameraArmComponent{nullptr},
+	CameraComponent{nullptr}
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGasCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AGasCharacterBase::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AGasCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
