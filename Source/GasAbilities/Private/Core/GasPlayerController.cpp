3c7178a8e88c1d92fa466175169f968967ca3204
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-10-30 19:03:36 +0100

    feat: add player controller class
    Add the player controller class derived from the player controller base class

---
+++ Source/GasAbilities/Private/Core/GasPlayerController.cpp
@@ -0,0 +1,4 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+
+#include "Core/GasPlayerController.h"
---
+++ Source/GasAbilities/Public/Core/GasPlayerController.h
@@ -0,0 +1,20 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/CharacterControllerBase/GasPlayerControllerBase.h"
+#include "GasPlayerController.generated.h"
+
+/**
+ * AGasPlayerController is the concrete Player Controller implementation derived from AGasPlayerControllerBase.
+ * It handles the full setup of player input and is specifically responsible for **binding input actions** * to the
+ * Character's Gameplay Ability System (GAS). 
+ * This class is the definitive link between player commands and the execution of Gameplay Abilities, 
+ * managing the runtime input configuration required by GAS.
+ */
+UCLASS()
+class GASABILITIES_API AGasPlayerController : public AGasPlayerControllerBase
+{
+	GENERATED_BODY()
+};
